90604a4de22089eba39663fa71dbf58c6244d1e7
Fedi Ghalloussi <62482405+FediGhalloussi@users.noreply.github.com>
2023-12-04 15:13:03 +0100

    16. ViewBinding

--- app/src/main/java/com/romainperrier/tp1/list/TaskListFragment.kt
+++ app/src/main/java/com/romainperrier/tp1/list/TaskListFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.romainperrier.tp1.R
+import com.romainperrier.tp1.databinding.FragmentTaskListBinding
 import java.util.UUID
 
 class TaskListFragment : Fragment() {
@@ -18,22 +19,24 @@ class TaskListFragment : Fragment() {
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
     )
-
+    private var _binding: FragmentTaskListBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_task_list, container, false)
+        _binding = FragmentTaskListBinding.inflate(inflater,container, false);
+        return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.task_list)
-        recyclerView.adapter = adapter
+        _binding?.taskList?.adapter = adapter
 
-        val floatingActionButton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
-        floatingActionButton.setOnClickListener {
+        _binding?.floatingActionButton?.setOnClickListener {
             val newTask = Task(id = UUID.randomUUID().toString(), title = "New task")
             taskList = taskList + newTask
             adapter.submitList(taskList)
